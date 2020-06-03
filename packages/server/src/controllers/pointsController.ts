72f7d285960cf5f19f34da6a5e0e969e802dc78c
Marcon Willian Oliveira Neves <marconwillian@protonmail.com>
2020-06-02 23:30:48 -0300

    Finish routes

--- packages/server/src/controllers/pointsController.ts
+++ packages/server/src/controllers/pointsController.ts
@@ -2,6 +2,42 @@ import {Request, Response} from 'express';
 import knex from './../database/connection';
 
 class PointsController {
+    async index(req: Request, res: Response){
+        const {city, uf, items} = req.query;
+
+
+        const pasedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+            const points = await knex('points')
+                .join('pint_items', 'points.id', '=', 'pint_items.point_id')
+                .whereIn('pint_items.item_id', pasedItems)
+                .where('city', String(city))
+                .where('uf', String(uf))
+                .distinct()
+                .select('points.*');
+
+        return res.status(200).json(points);
+    }
+
+    async show(req: Request, res: Response){
+        const { _id } = req.params;
+        
+        const point = await knex('points').where('id', _id).first();
+
+        if(!point) {
+            return res.status(400).json({message: 'Point not found.'})
+        }
+
+        const items = await knex('items')
+            .join('pint_items', 'items.id', '=', 'pint_items.item_id')
+            .where('pint_items.point_id', _id)
+            .select('items.title');
+        
+        return res.status(200).json({point, items});
+    }
+
     async create(req: Request, res: Response){
         const {
             name,
@@ -40,6 +76,8 @@ class PointsController {
     
         await trx('pint_items').insert(pointItems);
     
+        await trx.commit();
+
         return res.status(200).json({
             id:  point_id,
             ...point
--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -5,6 +5,7 @@ import knex from './database/connection';
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
 
+// index, show, create, update, delete
 
 const routes = express.Router();
 const pointsController = new PointsController();
@@ -13,6 +14,8 @@ const itemsController = new ItemsController();
 routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:_id', pointsController.show);
 routes.post('/points', pointsController.create);
 
 
