176362e8a7351e1525e141a76c41ed6c421bbb41
Marcon Willian Oliveira Neves <marconwillian@protonmail.com>
2020-06-02 22:04:36 -0300

    Config routes List Itens

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import path from 'path';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users/:_id', (req, res) => {
-    return res.status(200).json({});
+routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            imageUrl: `https://ecoleta-backend.herokuapp.com/uploads/${item.image}`
+        }
+    })
+
+    return res.status(200).json(serializedItems);
 });
+
+
+
 
 export default routes;
