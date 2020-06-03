0a817254a23a8599fc35dc88d4d6545f022dee13
Marcon Willian Oliveira Neves <marconwillian@protonmail.com>
2020-06-02 22:21:06 -0300

    0.1.9 - Add route of create point

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -20,6 +20,44 @@ routes.get('/items', async (req, res) => {
     return res.status(200).json(serializedItems);
 });
 
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    const trx = await knex.transaction();
+    
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+    const point_id = insertedIds[0];
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+    })
+
+    await trx('pint_items').insert(pointItems);
+
+    return res.status(200).json({sucess: true})
+
+});
+
 
 
 
