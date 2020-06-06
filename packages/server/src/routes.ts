34aa1636f740600dc197a5be35550595d70b35f3
Marcon Willian <marconwillian@protonmail.com>
2020-06-06 18:45:52 -0300

    Finish backend

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {celebrate, Joi} from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -22,7 +23,24 @@ routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:_id', pointsController.show);
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post('/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    },{
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 
 
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -35,6 +36,7 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(errors());
 app.use((req, res) => {
     res.status(404).json({error: "Sorry can't find that!"})
 })
