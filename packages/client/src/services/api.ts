446fa39504d31fe608d7112c3f681ec318e243bc
Marcon Willian <marconwillian@protonmail.com>
2020-08-06 14:36:53 -0300

    cliente - 0.1.1 | Update api

--- packages/client/src/services/api.ts
+++ packages/client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ecoleta-backend.herokuapp.com:3333'
+    baseURL: 'https://server.ecoleta.marconwillian.dev'
 });
 
 export default api;
