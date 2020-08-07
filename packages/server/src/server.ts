68cf2774b8da6e38fecebc1437ed65decf3976af
Marcon Willian <marconwillian@protonmail.com>
2020-08-07 08:55:37 -0300

    Config Dotenv

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import routes from './routes';
 import { errors } from 'celebrate';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
