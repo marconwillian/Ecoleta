3e14735895283d6dbd5bae2ad0487918406cd9c8
Marcon MH4SH <marcon@mh4sh.dev>
2020-12-06 17:48:20 -0300

    Update projext

--- packages/server/src/database/migrations/02_create_pint_items.ts
+++ packages/server/src/database/migrations/02_create_pint_items.ts
@@ -2,15 +2,14 @@ import Knex from 'knex';
 
 export async function up(knek: Knex) {
     return knek.schema.createTable('pint_items', table => {
-        table.increments('id').primary(),
-        table.integer('point_id')
-            .notNullable()
-            .references('id')
-            .inTable('points'),
-        table.integer('item_id')
-            .notNullable()
-            .references('id')
-            .inTable('items');
+        table.increments('id').primary();
+
+
+        table.integer('point_id').unsigned().notNullable();
+        table.foreign('point_id').references('id').inTable('points');
+
+        table.integer('item_id').unsigned().notNullable();
+        table.foreign('item_id').references('id').inTable('items');
     })
 }
 
