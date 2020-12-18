7817540cd430141211b0636c4a7577470cc5598c
Marcon Willian <marconwillian@protonmail.com>
2020-12-17 21:02:16 -0300

    chore: Update env in vercel

---
+++ client/src/config/service.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    url: "develop-ecoleta.marconwillian.dev",
+    endpoint: "server-ecoleta.marconwillian.dev:8080"
+  },
+  {
+    url: "ecoleta.marconwillian.dev",
+    endpoint: "server-ecoleta.marconwillian.dev"
+  }
+];
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+console.log(process.env.VERCEL_URL);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
