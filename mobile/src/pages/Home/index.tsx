b23173f47bc37ea0f9667f09124c75b4b3e658d2
Marcon Willian <marconwillian@protonmail.com>
2020-12-19 22:51:27 -0300

    chore: Change input for picker

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,19 +1,56 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
 import { View, ImageBackground, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
+import api from '../../services/api';
 
+interface States {
+  uf: string;
+}
+
+interface Cities {
+  uf: string;
+  cities: {
+    name: string;
+  }[]
+}
 
 const Home = () => {
   const navigation = useNavigation();
 
-  const [uf, setUf] = useState('');
-  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('0');
+  const [ufs, setUfs] = useState([] as string[]);
+  const [city, setCity] = useState('0');
+  const [cities, setCities] = useState([] as string[]);
 
   function handleNavigateToPoints() {
     navigation.navigate('Points', {uf, city})
   }
+
+  useEffect(() => {
+    setUfs([]);
+    setCity('loading');
+    api.get<States[]>('points/states')
+        .then(response => {
+            const ufInitials = response.data.map(state => { 
+                return state.uf
+            })
+            setCity('0');
+            setUfs(ufInitials);
+        })
+  }, [])
+
+  useEffect(() => {
+    api.get<Cities>(`points/states/${uf}/cities`)
+        .then(response => {
+            const citiesSelected = response.data.cities.map(state => { 
+                return state.name
+            })
+            setCities(citiesSelected);
+        })
+  }, [uf])
   
   return (
     <KeyboardAvoidingView
@@ -32,22 +69,41 @@ const Home = () => {
         </View>
         
         <View style={styles.footer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Digite a UF"
-            maxLength={2}
-            autoCapitalize="characters"
-            autoCorrect={false}
-            value={uf}
-            onChangeText={setUf}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Digite a Cidade"
-            autoCorrect={false}
-            value={city}
-            onChangeText={setCity}
-          />
+          <View
+            style={{...styles.inputView}}
+          >
+            <Picker
+              style={{color: uf === '0' ? "#949494" : "#464646" ,...styles.input}}
+              selectedValue={uf}
+              onValueChange={(itemValue, itemIndex) => {
+                setUf(`${itemValue}`);
+              }
+            }>
+              <Picker.Item label="Selecione seu Estado" value="0"/>
+              { ufs.map(uf => (
+                  <Picker.Item label={uf} value={uf}/>
+              ))}
+            </Picker>
+          </View>
+          
+          <View
+            style={{...styles.inputView}}
+          >
+            <Picker
+              style={{color: city === '0' ? "#949494" : "#464646" ,...styles.input}}
+              selectedValue={city}
+              enabled={cities.length!==0}
+              onValueChange={(itemValue, itemIndex) => {
+                setCity(`${itemValue}`);
+              }
+            }>
+              <Picker.Item label="Selecione sua Cidade" value="0" />
+              { city==="loading" && <Picker.Item label="carregando.." value="loading" /> }
+              { cities.map(city => (
+                  <Picker.Item label={city} value={city}/>
+              ))}
+            </Picker>
+          </View>
 
           <RectButton style={styles.button} onPress={handleNavigateToPoints}>
             <View style={styles.buttonIcon}>
@@ -99,15 +155,17 @@ const styles = StyleSheet.create({
 
   select: {},
 
-  input: {
+  inputView: {
     height: 60,
     backgroundColor: '#FFF',
     borderRadius: 10,
     marginBottom: 8,
-    paddingHorizontal: 24,
+    paddingHorizontal: 12,
     fontSize: 16,
   },
-
+  input: {
+    height: 60
+  },
   button: {
     backgroundColor: '#34CB79',
     height: 60,
