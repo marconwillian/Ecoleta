17964c1cae0ecc954384c8942010a20a7afa8d47
Marcon Willian <oi@marconwillian.dev>
2021-03-21 21:26:37 -0300

    fix 🐛: Fix css and migrations

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knek: Knex) {
     return knek.schema.createTable('items', table => {
-        table.increments('id').primary(),
+        table.increments('id').primary();
         table.string('image').notNullable();
         table.string('title').notNullable();
     })
