55071f1fac7cda2995e0a71425c5b4eab2e8ebe2
Marcon Willian <oi@marconwillian.dev>
2021-03-21 22:28:03 -0300

    fix 🐛: Remove duplication of knexfile

---
+++ server/src/config/knexConfig.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import 'dotenv/config';
+
+export default  {
+    development: {
+        migrations: {
+            directory: path.resolve(__dirname, '..', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, '..', 'database', 'seeds')
+        },
+        client: 'mysql',
+        connection: {
+            host : process.env.DB_HOST,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASSWORD,
+            database : process.env.DB_DATABASE
+        }
+    },
+    test: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, '..', 'database', 'dev.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, '..', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, '..', 'database', 'seeds')
+        },
+        useNullAsDefault: false
+    },
+    production: {
+        migrations: {
+            directory: path.resolve(__dirname, '..', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, '..', 'database', 'seeds')
+        },
+        client: 'mysql',
+        connection: {
+            host : process.env.DB_HOST,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASSWORD,
+            database : process.env.DB_DATABASE
+        }
+    }
+};
